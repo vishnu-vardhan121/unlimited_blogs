522efef368470c71b4994bc51d0d7184d47c7214
vishnu-vardhan121 <kottevishnuvardhann@gmail.com>
2025-03-17 14:48:06 +0530

    spelling mistake in import file name

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 
 function App() {
   return (
